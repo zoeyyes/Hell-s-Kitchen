52b2e2aaffe90dbe34f609fbf0ab5c502d04b901
robinyeohood <robinyeo.hood@gmail.com>
2022-07-18 18:43:20 +0800

    Updated generate_random_demand() to differentiate demand on Fridays & Saturdays from other days of the week.

--- Robin_Helper_Functions.R
+++ Robin_Helper_Functions.R
@@ -9,6 +9,7 @@ generate_random_demand <- function(round_number, demand_df) {
   
   # For Monday to Thursday of every round,
   for (day in (7 * round_number - 6):(7 * round_number - 3)) {
+    
     # Generate demand for Dish 1: Mixed Vegetable Rice Set A
     mixed_veg_rice_set_A <- round(rnorm(1, mean = 40, sd = 5))
     while (mixed_veg_rice_set_A <= 0) {
@@ -30,6 +31,7 @@ generate_random_demand <- function(round_number, demand_df) {
   
   # For Friday and Saturday of every round,
   for (day in (7 * round_number - 2):(7 * round_number - 1)) {
+    
     # Generate demand for Dish 1: Mixed Vegetable Rice Set A
     mixed_veg_rice_set_A <- round(rnorm(1, mean = 80, sd = 10))
     while (mixed_veg_rice_set_A <= 0) {
@@ -54,15 +56,15 @@ generate_random_demand <- function(round_number, demand_df) {
   
 }
 
-# Debugging: Create a test demand_df
-demand_test_df <- data.frame(Day = c(1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13),
-                             Mixed_Vegetable_Rice_Set_A = c(rep(0, 12)),
-                             Mixed_Vegetable_Rice_Set_B = c(rep(0, 12)))
-
-# Debugging: Generate demand for Round 1
-demand_test_df <- generate_random_demand(1, demand_test_df)
-print(demand_test_df)
-
-# Debugging: Generate demand for Round 2
-demand_test_df <- generate_random_demand(2, demand_test_df)
-print(demand_test_df)
+# # Debugging: Create a test demand_df
+# demand_test_df <- data.frame(Day = c(1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13),
+#                              Mixed_Vegetable_Rice_Set_A = c(rep(0, 12)),
+#                              Mixed_Vegetable_Rice_Set_B = c(rep(0, 12)))
+# 
+# # Debugging: Generate demand for Round 1
+# demand_test_df <- generate_random_demand(1, demand_test_df)
+# print(demand_test_df)
+# 
+# # Debugging: Generate demand for Round 2
+# demand_test_df <- generate_random_demand(2, demand_test_df)
+# print(demand_test_df)
