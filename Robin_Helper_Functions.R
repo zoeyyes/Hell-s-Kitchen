aabed240dcd076a01d4dc1c254d307b43b7a592b
robinyeohood <robinyeo.hood@gmail.com>
2022-07-18 17:41:31 +0800

    18 Jul 2022, 1741H: Robin updated testing his helper functions.

---
+++ Robin_Helper_Functions.R
@@ -0,0 +1,26 @@
+# This function generates random demand for each of the dishes
+# round_number: depending on the round_number, we know which dishes will need non-zero demand
+# (e.g. in Round 1, Dish 5 will have no demand but will have non-zero demand in Round 4)
+# demand_df: this is the demand data frame that needs to be updated
+# HOW TO USE: demand <- function(1, demand)
+# This will update your demand for dishes in Round 1
+generate_random_demand <- function(round_number, demand_df) {
+  
+  # Generates demand for Dish 1: Mixed Vegetable Rice Set A
+  mixed_veg_rice_set_A <- round(rnorm(1, mean = 40, sd = 5))
+  while (mixed_veg_rice_set_A <= 0) {
+    mixed_veg_rice_set_A <- round(rnorm(1, mean = 40, sd = 5))
+  }
+  # print(mixed_veg_rice_set_A) # For debugging
+  demand_df[demand_df$round == round_number, 'Mixed_Vegetable_Rice_Set_A'] = mixed_veg_rice_set_A
+  # print(demand_df) # For debugging
+  
+}
+
+# # Debugging: Create a test demand_df
+# demand_test_df <- data.frame(round = c(1, 2),
+#                              Mixed_Vegetable_Rice_Set_A = c(0, 0),
+#                              Mixed_Vegetable_Rice_Set_B = c(0, 0))
+# 
+# #demand_test_df <- generate_random_demand(1, demand_test_df)
+# generate_random_demand(1, demand_test_df)
