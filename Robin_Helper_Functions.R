09bedf6f639bddd0cdfc7fb724ac7d6eb5c71ff8
robinyeohood <robinyeo.hood@gmail.com>
2022-07-18 18:40:53 +0800

    Updated generate_random_demand() to differentiate demand on Fridays & Saturdays from other days of the week.

--- Robin_Helper_Functions.R
+++ Robin_Helper_Functions.R
@@ -7,21 +7,46 @@
 # This will update your demand for dishes in Round 1
 generate_random_demand <- function(round_number, demand_df) {
   
-  # Generates demand for Dish 1: Mixed Vegetable Rice Set A
-  mixed_veg_rice_set_A <- round(rnorm(1, mean = 40, sd = 5))
-  while (mixed_veg_rice_set_A <= 0) {
+  # For Monday to Thursday of every round,
+  for (day in (7 * round_number - 6):(7 * round_number - 3)) {
+    # Generate demand for Dish 1: Mixed Vegetable Rice Set A
     mixed_veg_rice_set_A <- round(rnorm(1, mean = 40, sd = 5))
-  }
-  # print(mixed_veg_rice_set_A) # For debugging
-  demand_df[demand_df$round == round_number, 'Mixed_Vegetable_Rice_Set_A'] = mixed_veg_rice_set_A
-  # print(demand_df) # For debugging
-  
-  if (round_number >= 2) {
-    mixed_veg_rice_set_B <- round(rnorm(1, mean = 40, sd = 5))
-    while (mixed_veg_rice_set_B <= 0) {
-      mixed_veg_rice_set_B <- round(rnorm(1, mean = 40, sd = 5))
+    while (mixed_veg_rice_set_A <= 0) {
+      mixed_veg_rice_set_A <- round(rnorm(1, mean = 40, sd = 5))
     }
-    demand_df[demand_df$round == round_number, 'Mixed_Vegetable_Rice_Set_B'] = mixed_veg_rice_set_B
+    # print(mixed_veg_rice_set_A) # For debugging
+    demand_df[demand_df$Day == day, 'Mixed_Vegetable_Rice_Set_A'] = mixed_veg_rice_set_A
+    # print(demand_df) # For debugging
+    
+    # Generate demand for Dish 2: Mixed Vegetable Rice Set B
+    if (round_number >= 2) {
+      mixed_veg_rice_set_B <- round(rnorm(1, mean = 40, sd = 5))
+      while (mixed_veg_rice_set_B <= 0) {
+        mixed_veg_rice_set_B <- round(rnorm(1, mean = 40, sd = 5))
+      }
+      demand_df[demand_df$Day == day, 'Mixed_Vegetable_Rice_Set_B'] = mixed_veg_rice_set_B
+    }
+  }
+  
+  # For Friday and Saturday of every round,
+  for (day in (7 * round_number - 2):(7 * round_number - 1)) {
+    # Generate demand for Dish 1: Mixed Vegetable Rice Set A
+    mixed_veg_rice_set_A <- round(rnorm(1, mean = 80, sd = 10))
+    while (mixed_veg_rice_set_A <= 0) {
+      mixed_veg_rice_set_A <- round(rnorm(1, mean = 80, sd = 10))
+    }
+    # print(mixed_veg_rice_set_A) # For debugging
+    demand_df[demand_df$Day == day, 'Mixed_Vegetable_Rice_Set_A'] = mixed_veg_rice_set_A
+    # print(demand_df) # For debugging
+    
+    # Generate demand for Dish 2: Mixed Vegetable Rice Set B
+    if (round_number >= 2) {
+      mixed_veg_rice_set_B <- round(rnorm(1, mean = 80, sd = 10))
+      while (mixed_veg_rice_set_B <= 0) {
+        mixed_veg_rice_set_B <- round(rnorm(1, mean = 80, sd = 10))
+      }
+      demand_df[demand_df$Day == day, 'Mixed_Vegetable_Rice_Set_B'] = mixed_veg_rice_set_B
+    }
   }
   
   # Return the update demand data frame
@@ -29,15 +54,15 @@ generate_random_demand <- function(round_number, demand_df) {
   
 }
 
-# # Debugging: Create a test demand_df
-# demand_test_df <- data.frame(round = c(1, 2),
-#                              Mixed_Vegetable_Rice_Set_A = c(0, 0),
-#                              Mixed_Vegetable_Rice_Set_B = c(0, 0))
-# 
-# # Debugging: Generate demand for Round 1
-# demand_test_df <- generate_random_demand(1, demand_test_df)
-# print(demand_test_df)
-# 
-# # # Debugging: Generate demand for Round 2
-# demand_test_df <- generate_random_demand(2, demand_test_df)
-# print(demand_test_df)
+# Debugging: Create a test demand_df
+demand_test_df <- data.frame(Day = c(1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13),
+                             Mixed_Vegetable_Rice_Set_A = c(rep(0, 12)),
+                             Mixed_Vegetable_Rice_Set_B = c(rep(0, 12)))
+
+# Debugging: Generate demand for Round 1
+demand_test_df <- generate_random_demand(1, demand_test_df)
+print(demand_test_df)
+
+# Debugging: Generate demand for Round 2
+demand_test_df <- generate_random_demand(2, demand_test_df)
+print(demand_test_df)
