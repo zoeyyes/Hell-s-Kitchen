efd9693ecc7e600aa0569a0559c57eec5321d473
robinyeohood <robinyeo.hood@gmail.com>
2022-07-20 22:42:59 +0800

    Updated update_records(), publish_leaderboard(), and clear_record()

--- Robin_End_Game_Helper_Functions.R
+++ Robin_End_Game_Helper_Functions.R
@@ -1,5 +1,5 @@
 # At the beginning of any R session, record your AWS database password:
-source("setAWSPassword.R")
+# source("setAWSPassword.R")
 
 source("usePackages.R")
 loadPkgs(c("shiny","DBI","stringr"))
@@ -25,13 +25,13 @@ getAWSConnection_robin <- function(){
   conn
 }
 
-# update_records()
+# WORKING
 # Input: (playername, cash-on-hand) 
 # Update cash-on-hand as scores to our database
-update_records <- function(playerid, cash_on_hand) {
+update_records <- function(playername, cash_on_hand) {
   conn <- getAWSConnection_robin()
-  querytemplate <- "INSERT INTO LeaderScore_test (playerid, cash_on_hand, asoftime) VALUES (?id1, ?id2, NOW())"
-  query <- sqlInterpolate(conn, querytemplate,id1 = playerid,id2 = cash_on_hand)
+  querytemplate <- "INSERT INTO LeaderScore_test (playername, cash_on_hand, asoftime) VALUES (?id1, ?id2, NOW())"
+  query <- sqlInterpolate(conn, querytemplate,id1 = playername,id2 = cash_on_hand)
   print(query) #for debug
   success <- FALSE
   tryCatch(
@@ -50,13 +50,43 @@ update_records <- function(playerid, cash_on_hand) {
 }
 
 ### Testing update_records() ###
+test_playername <- "test1"
+test_cash_on_hand <- 10800
+update_records(test_playername, test_cash_on_hand)
+### Test End ###
 
+# WORKING
 # publish_leaderboard() 
-# Input: (playerid) 
-# Output:leaderboard 
+# Input: none
+# Output:leaderboard
+publish_leaderboard <- function() {
+  conn <- getAWSConnection_robin()
+  # Extract columns playername and cash_on_hand
+  # ORDER BY cash_on_hand_DESC
+  # Show just the top 5 scores
+  result <- dbGetQuery(conn, "SELECT playername, cash_on_hand FROM LeaderScore_test ORDER BY cash_on_hand DESC LIMIT 5")
+  dbDisconnect(conn)
+  
+  # return the leader board
+  result
+}
 
+### Testing publish_leaderboard() ###
+publish_leaderboard()
+### Test End ###
 
 
 # clear_record(if restart) 
-# input:(playername) 
-# clear the records of the player in our database 
+# input: (playername) 
+# clear the records of the player in our database
+clear_record <- function(playername) {
+  conn <- getAWSConnection_robin()
+  query <- paste0("DELETE FROM LeaderScore_test WHERE playername = '", playername, "'")
+  dbGetQuery(conn, query)
+  dbDisconnect(conn)
+}
+
+### Testing publish_leaderboard() ###
+test6 <- "test6"
+clear_record(test6)
+### Test End ###
